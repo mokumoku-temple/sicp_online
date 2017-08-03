0d4b54cb926f536ee1b1d973c2bc7253644a18fc
KazumiHarada <kazumi.h.apple48@gmail.com>
2017-08-03 22:41:38 +0900

    tt

--- 1 Building Abstractions with Procedures/kazu04r.scm
+++ 1 Building Abstractions with Procedures/kazu04r.scm
@@ -54,8 +54,25 @@
 
 (define (g n) (A 1 n)) -> (A 0 (A 1 (- n 1))) -> (* 2 (A 1 (- n 1))) -> (* 2 (A 0 (A 1 (- (- n 1) 1)))) -> (* 2 (* 2 (A 1 (- (- n 1) 1)))) -> 2^n
 
-(define (h n) (A 2 n)) -> (A 1 (A 2 (- n 1))) -> (^ 2 (A 2 (- n 1))) -> (^ 2 (A 1 (A 2 (- (- n 1) 1)))) -> (^ 2 (^ 2 (A 2 (- (- n 1) 1)))) -> 2^n^n == (A 1 16)
+(define (h n) (A 2 n)) -> (A 1 (A 2 (- n 1))) -> (^ 2 (A 2 (- n 1))) -> (^ 2 (A 1 (A 2 (- (- n 1) 1)))) -> (^ 2 (^ 2 (A 2 (- (- n 1) 1)))) -> 2^n^n == (A 1 (* n n))
 
 (define (k n) (* 5 n n))
 
+;;------------------
+;; 1.11
+;;------------------
+
+(define fun11recur
+  (lambda (n)
+    (cond
+     ((< n 3) n)
+     (else (+ (fun11recur (- n 1)) (* 2 (fun11recur (- n 2))) (* 3 (fun11recur (- n 3))))))))
+
+(define fun11iter
+  (lambda (n)
+    (let loop ((n n) (a a) (b b) (c c))
+      (cond
+       ((< n 3) n)
+       (else (loop (- n 1) (fun11recur (- n 1)) (* 2 (fun11recur (- n 2))) (* 3 (fun11recur (- n 3))))))))
+      
 
